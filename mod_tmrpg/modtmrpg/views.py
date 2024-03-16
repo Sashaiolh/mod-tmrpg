4941fa163464c706fdb2d4cab6d227b59bee76f7
Sasha2319 <dliapromua@gmail.com>
2024-03-16 15:18:32 +0200

    при покупке плюс лог

--- mod_tmrpg/modtmrpg/views.py
+++ mod_tmrpg/modtmrpg/views.py
@@ -41,8 +41,8 @@ CLIENT_SECRET = '11ZYkGhlOqRTLXl64oPxWBOETKdoQw0m'
 REDIRECT_URI = "http://128.0.0.1:8000/oauth2/discord/"
 
 
-# webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1213640643558117446/5dUk-tlebu7QfT6XJ35vM7Z0vGDjhPwgwjiWYRwbY7cAnHn6NrQD_E4vrdy7qlYq-zz3")   
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1217123338753933352/tFgGpj921OWFiEMmrbbsyHyEWGaCl7L3GbwM2fPeDcC43J8oBReFRKF7V1QJ-aJpnDW-") 
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1213640643558117446/5dUk-tlebu7QfT6XJ35vM7Z0vGDjhPwgwjiWYRwbY7cAnHn6NrQD_E4vrdy7qlYq-zz3")   
+# webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1217123338753933352/tFgGpj921OWFiEMmrbbsyHyEWGaCl7L3GbwM2fPeDcC43J8oBReFRKF7V1QJ-aJpnDW-") 
 
 # def exchange_code(code):
 #   data = {
@@ -444,6 +444,9 @@ def buy_item(request, id):
     else:
         moder.balance -= item.price*amount
         moder.save()
+
+        new_log = models.EcoLog.objects.create(moder=moder, amount=item.price*-1, reason=f'Покупка {item.item_name}')
+        new_log.save()
 
         curatorPex = models.Pex.objects.get(pex_name='curator')
         curatorsQuery = models.Moder.objects.filter(pex=curatorPex)
