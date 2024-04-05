d5ef2c0619f847e641e6c31d0ca1a3887b6d6094
Sasha2319 <dliapromua@gmail.com>
2024-04-05 16:52:22 +0300

    asdasdasdasd
    asdasdasdasd

--- mod_tmrpg/modtmrpg/api/MacroKeyBind/api_mkb.py
+++ mod_tmrpg/modtmrpg/api/MacroKeyBind/api_mkb.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def api_macrokb_playtimereport(request):
     if request.method == 'POST':
-        text = 'Raw Data: "%s"' % request.raw_post_data
+        text = "%s %s" % (request.method, request.body)
         config = Config.objects.get(config_name='playtimeTest')
         config.config = text
         config.save()
